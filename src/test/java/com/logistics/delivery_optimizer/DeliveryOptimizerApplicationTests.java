beb3859843ebfb6dc8e774201217b7ec0a81132c
Vivek9874 <vivektiwaripr@gmail.com>
2025-08-30 21:05:01 +0530

    Initial Commit

---
+++ src/test/java/com/logistics/delivery_optimizer/DeliveryOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.delivery_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

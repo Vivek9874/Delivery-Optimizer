235a7f61e47c11e17bfed69d87bef60d0e29ecd4
Vivek9874 <vivektiwaripr@gmail.com>
2025-09-20 18:22:03 +0530

    feat: Add Geocoding to OrderService
    Integrate GeocodingService to automatically fetch and set latitude and longitude
    for a new order before it's persisted to the database. This ensures all new
    orders have geographical coordinates for use in future location-based features
    like route optimization.

--- src/main/java/com/logistics/delivery_optimizer/Service/OrderService.java
+++ src/main/java/com/logistics/delivery_optimizer/Service/OrderService.java
@@ -1,4 +1,53 @@
 package com.logistics.delivery_optimizer.Service;
 
+import com.logistics.delivery_optimizer.Model.Order;
+import com.logistics.delivery_optimizer.Repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    private  final OrderRepository orderRepository;
+    private final GeocodingService geocodingService;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository, GeocodingService geocodingService) {
+        this.orderRepository = orderRepository;
+        this.geocodingService = geocodingService;
+    }
+
+    public Order createOrder(Order order){
+
+        Optional<double[]> coordinates = geocodingService.getCoordinates(order.getAddress());
+
+        if(coordinates.isPresent()){
+            double[] latLon = coordinates.get();
+            order.setLatitude(latLon[0]);
+            order.setLongitude(latLon[1]);
+        }
+        else {
+            System.err.println("Geocoding failed for address: " + order.getAddress());
+        }
+        return orderRepository.save(order);
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        if (!orderRepository.existsById(id)) {
+            System.err.println("Attempted to delete a non-existent order with ID: " + id);
+        }
+        orderRepository.deleteById(id);
+    }
 }

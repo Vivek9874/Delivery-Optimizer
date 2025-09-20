dc4de837f348521913d84fd58ea65535ccf9bae0
Vivek9874 <vivektiwaripr@gmail.com>
2025-09-20 18:14:51 +0530

    Implemented an interface, which has a findByStatus method.

--- src/main/java/com/logistics/delivery_optimizer/Repository/OrderRepository.java
+++ src/main/java/com/logistics/delivery_optimizer/Repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package com.logistics.delivery_optimizer.Repository;
 
-public class OrderRepository {
+import com.logistics.delivery_optimizer.Model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStatus(Order.Status status);
 }
